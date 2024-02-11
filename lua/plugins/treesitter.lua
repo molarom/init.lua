1207def59b588540a2cdbb51e934eb9f97164428
molarom <brandon.epperson@protonmail.com>
2024-02-11 00:01:32 -0500

    add: treesitter-text-objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    depends = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     enabled = true,
     version = false,
     build = ':TSUpdate',
