396fc02c7ef5131dced955e43fb3f85efeb17019
molarom <brandon.epperson@protonmail.com>
2024-02-11 00:00:59 -0500

    tweak: search highlighting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -50,3 +50,6 @@ vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" 
 vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
 vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
 vim.keymap.set("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+-- Search highlights
+vim.keymap.set("n", "<C-l>", "<cmd>noh<cr>", { desc = "Clear highlights" })
