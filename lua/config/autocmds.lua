23832e647fae665d01b0534b01d0ee64faa3c53e
molarom <brandon.epperson@protonmail.com>
2024-02-10 23:54:22 -0500

    fix: highlight yanked text

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,16 +4,16 @@ local augroup = vim.api.nvim_create_augroup
 -- Create autogroup.
 local molaromAG = augroup('molarom', { clear = true })
 
--- autocmd('TextYankPost', {
---   callback = function()
---     require("vim.highlight").on_yank({ higroup = "YankHighlight", timeout = 200 })
---   end,
---   group = molaromAG,
---   desc = "Highlight when yanking",
---   pattern = '*',
--- })
+autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = molaromAG,
+  desc = "Highlight when yanking",
+  pattern = '*',
+})
 
-autocmd({ "VimEnter" }, { 
+autocmd({ "VimEnter" }, {
   callback = function()
     local ok, _ = pcall(require, "nvim-tree")
     if(not ok) then
@@ -43,41 +43,3 @@ autocmd("TermOpen", {
   group = molaromAG,
   desc = "Terminal Options",
 })
-
-
--- autocmd("WinClosed", {
---   callback = function ()
---     local winnr = tonumber(vim.fn.expand("<amatch>"))
---     vim.schedule_wrap(function (winnr)
---         local api = require"nvim-tree.api"
---         local tabnr = vim.api.nvim_win_get_tabpage(winnr)
---         local bufnr = vim.api.nvim_win_get_buf(winnr)
---         local buf_info = vim.fn.getbufinfo(bufnr)[1]
---         local tab_wins = vim.tbl_filter(function(w) return w~=winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
---         local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
---         if buf_info.name:match(".*NvimTree_%d*$") then            -- close buffer was nvim tree
---           -- Close all nvim tree on :q
---           if not vim.tbl_isempty(tab_bufs) then                      -- and was not the last window (not closed automatically by code below)
---             api.tree.close()
---           end
---         else                                                      -- else closed buffer was normal buffer
---           if #tab_bufs == 1 then                                    -- if there is only 1 buffer left in the tab
---             local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
---             if last_buf_info.name:match(".*NvimTree_%d*$") then       -- and that buffer is nvim tree
---               vim.schedule(function ()
---                 if #vim.api.nvim_list_wins() == 1 then                -- if its the last buffer in vim
---                   vim.cmd "quit"                                        -- then close all of vim
---                 else                                                  -- else there are more tabs open
---                   vim.api.nvim_win_close(tab_wins[1], true)             -- then close only the tab
---                 end
---               end)
---             end
---           end
---         end
---       end
---     )
---   end,
---   nested = true,
---   group = molaromAG,
---   desc = "Close NvimTree if it's the last buffer.",
--- })
